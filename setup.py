fa6ff3185dc482b5bda6e3a9f6f2080f11043676
NightlyShelf <shchurartempm@hotmail.com>
2022-05-11 16:34:36 +0300

    Correcting work

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="locass",
-    version="alpha-1.0",
+    version="1.0.0",
     author="NightlyShelf",
     description="Simple localization manager",
     long_description=long_description,
