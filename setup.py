0fad4a51619f3d092197218f76d4df7643e0657f
NightlyShelf <shchurartempm@hotmail.com>
2022-05-11 15:18:30 +0300

    Correct version appeal

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="locass",
-    version="v.1.0-beta",
+    version="beta1.0",
     author="NightlyShelf",
     description="Simple localization manager",
     long_description=long_description,
